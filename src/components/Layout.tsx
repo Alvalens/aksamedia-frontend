35a2eeb01923b737f2500ca121313391c851bd7b
Alvalen <alvalen.shafel04@gmail.com>
2025-01-09 11:52:37 +0700

    fix: table responsiveness

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout: React.FC = () => {
     return (
         <div className="flex h-screen">
             <Sidebar />
-            <div className="flex-1 flex flex-col">
+            <div className="flex-1 flex flex-col overflow-hidden">
                 <Navbar />
                 <main className="p-4 flex-1 bg-gray-100 dark:bg-gray-700 ps-20 md:ps-4">
                     <Outlet />
--- src/pages/employee/Index.tsx
+++ src/pages/employee/Index.tsx
@@ -51,7 +51,7 @@ const Index: React.FC = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-auto p-4 bg-white dark:bg-gray-600 shadow-sm rounded-md overflow-hidden">
+    <div className="max-w-7xl mx-auto p-4 bg-white dark:bg-gray-600 shadow-sm rounded-md">
       <h1 className="text-2xl font-bold mb-4 dark:text-white">Employee List</h1>
       <div className="flex justify-between items-center mb-4">
         <Link to="create" className="bg-blue-500 text-white py-2 px-4 rounded">
@@ -68,9 +68,9 @@ const Index: React.FC = () => {
           }}
         />
       </div>
-      <div className="overflow-x-auto w-full">
-        <div className='inline-block min-w-full '>
-          <table className="min-w-full dark:text-white">
+      <div className="relative">
+        <div className="overflow-x-auto border rounded-lg">
+          <table className="w-full table-auto">
             <thead>
               <tr className="bg-gray-100 dark:bg-gray-700 dark:text-white">
                 <th className="px-4 py-2 text-left">#</th>
